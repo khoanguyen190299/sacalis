dc7c0b3ba9b988ddc35e0d5962c51c3cdbea82e0
Khoa.nguyen <khoanguyen190299@gmail.com>
2023-08-02 19:33:40 +0700

    adding dist subtree

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
-const sass = require('sass')
 module.exports = defineConfig({
   transpileDependencies: true,
-  // publicPath: process.env.NODE_ENV === 'production'
-  //   ? '/sacalis-app/'
-  //   : '/'
+  publicPath: process.env.NODE_ENV === "production" ? "/REPO_NAME/" : "/"
 })
